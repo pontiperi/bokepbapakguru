00cbfa7e2459e6696384ec11ea9985d29620e62b
pontiperi <pontiperi@gmail.com>
2025-07-10 17:12:38 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bokepbapakguru
-bokepbapakguru
+# [bokepbapakguru](https://bokepbapakguru.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const url = 'https://bokepbapakguru.pages.dev';
+export const nama = 'Bokep Bapak Guru';
+export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante';
+export const terbit = '2025-01-25T00:01:21Z';
+
+export const google = '7IX-DGrx3YaiNIXOvLZsWlj-h0XlIjPIV06eZ9-qjzY';
+export const yandex = '52a59c84cf9d9e9b';
+
+export const index = '40a5f2fd-3429-4522-a47a-25d2d61e7afd';
